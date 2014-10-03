063136ff6d46387a9bac1868dc9777076ade3561
almaev <ilya.almaev@gmail.com>
2014-10-03 19:47:51 +0600

    vacancy screen

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,13 @@
 $(function(){
     getIndent();
+
+    $('.vacancy-list').accordion({ header: "h2", heightStyle: "content" });
 });
 $(window).resize(function(){ getIndent(); });
 $(window).scroll(function() {
 
     _pos = $(window).scrollTop();
-    _top = $('.vacancy').offset().top;
+    _top = $('.second').offset().top;
     if( _pos >= _top  )	{
         $('.contacts').addClass('show');
     }else{
@@ -15,7 +17,13 @@ $(window).scroll(function() {
 });
 
 function getIndent(){
+    var w = $(window).width();
     var h = $(window).height();
-    $('#second').css('margin-top', h);
-    $('#vacancy').css('margin-bottom', h);
+    $('#content_screen').css('margin-top', h).css('margin-bottom', h);
+    if( w <= 600) {
+        $('body').addClass('hide-navbar');
+    }else{
+        $('body').removeClass('hide-navbar');
+    }
+
 }
